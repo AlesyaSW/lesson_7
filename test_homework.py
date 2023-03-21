79ec809f7c280f29eda43e2edd03a166467dba8f
Alesya <kalesiaq@gmail.com>
2023-03-22 01:54:18 +0300

    добавление папки

--- test_homework.py
+++ test_homework.py
@@ -2,9 +2,11 @@ import zipfile
 import os
 import PyPDF2
 import openpyxl
+import pytest
 
 
-def test_zip_files():
+@pytest.fixture
+def zip_file():
     file_names = [os.path.abspath('file1.pdf'), os.path.abspath('file2.xlsx'), os.path.abspath('file3.csv')]
 
     zip_file = zipfile.ZipFile('resources/archive.zip', 'w')
@@ -12,18 +14,18 @@ def test_zip_files():
         if os.path.exists(file_name):
             zip_file.write(file_name, os.path.basename(file_name))
         else:
-            print(f"File {file_name} not found!")
+            print(f"Файл {file_name} не найден!")
     zip_file.close()
 
 
-def test_read_dpf():
+def test_read_dpf(zip_file):
     with zipfile.ZipFile('resources/archive.zip', 'r') as zip_file:
         with zip_file.open('file1.pdf', 'r') as pdf_file:
             pdf_reader = PyPDF2.PdfReader(pdf_file)
             assert len(pdf_reader.pages) == 10, "PDF file should have 10 pages."
 
 
-def test_read_csv():
+def test_read_csv(zip_file):
     with zipfile.ZipFile('resources/archive.zip', 'r') as zip_file:
         with zip_file.open('file3.csv', 'r') as csv_file:
             count_row = 0
@@ -32,7 +34,7 @@ def test_read_csv():
             assert 7 == count_row
 
 
-def test_read_xlsx():
+def test_read_xlsx(zip_file):
     with zipfile.ZipFile('resources/archive.zip', 'r') as zip_file:
         with zip_file.open('file2.xlsx', 'r') as xlsx_file:
             book = openpyxl.load_workbook(xlsx_file)
